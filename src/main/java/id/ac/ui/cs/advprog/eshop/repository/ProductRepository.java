7f596b83db0afb6f6293103edd85f8401235e42b
TheoKevH <theodorekevinh@gmail.com>
2025-02-12 00:30:46 +0700

    feat: add edit product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,41 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product edit(String productId, Product newProductData) {
+        Product productToEdit = findById(productId);
+
+        if (productToEdit != null) {
+            productToEdit.setProductName(newProductData.getProductName());
+            productToEdit.setProductQuantity(newProductData.getProductQuantity());
+        }
+
+        return productToEdit;
+    }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
     }
 }
